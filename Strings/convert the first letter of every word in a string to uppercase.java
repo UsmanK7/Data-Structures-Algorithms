d018fa36964e629b3d0950abc86a354cfed43970
Usman Khan <95441787+UsmanK7@users.noreply.github.com>
2023-03-16 20:48:42 +0500

    Add files via upload

---
+++ Strings/convert the first letter of every word in a string to uppercase.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Main {
+	public static void toUpperCase(String str) {
+		StringBuilder sb=new StringBuilder("");
+		char ch=Character.toUpperCase(str.charAt(0));
+		sb.append(ch);
+		
+		for(int i=1;i<str.length();i++) {
+			if(str.charAt(i)==' ' && i<str.length()-1) {
+				sb.append(str.charAt(i));
+				i++;
+				sb.append(Character.toUpperCase(str.charAt(i)));
+			}
+			else {
+				sb.append(str.charAt(i));
+			}
+		}
+		sb.toString();
+		System.out.println(sb);
+	}
+	public static void main(String[] args) {
+		String str="hi, i am usman";
+		toUpperCase(str);
+	}
+}
