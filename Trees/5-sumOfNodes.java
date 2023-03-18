1475f49c1edc2e71c259943036881133b79936d8
Usman Khan <95441787+UsmanK7@users.noreply.github.com>
2023-03-18 21:24:09 +0500

    Add files via upload

---
+++ Trees/5-sumOfNodes.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class BinaryTreeP {
+	static class Node{
+		int data;
+		Node left;
+		Node right;
+		Node(int data){
+			this.data=data;
+			this.left=null;
+			this.right=null;
+		}
+	}
+	static 	class BinaryTree{
+		static int idx=-1;
+		public static Node buildTree(int nodes[]) {
+			idx++;
+			if(nodes[idx]==-1) {
+				return null;
+			}
+			Node newNode=new Node(nodes[idx]);
+			newNode.left=buildTree(nodes);
+			newNode.right=buildTree(nodes);
+			return newNode;	
+		}
+	}
+	public static int sumOfNodes(Node root) {
+		if(root==null) {
+			return 0;
+		}
+		int LeftSum=sumOfNodes(root.left);
+		int RightSum=sumOfNodes(root.right);
+		return LeftSum+RightSum+root.data;
+	}
+	public static void main(String[] args) {
+		int nodes[]= {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+		BinaryTree tree=new BinaryTree();
+		Node root=tree.buildTree(nodes);
+		System.out.println(sumOfNodes(root));	
+	}
+}
